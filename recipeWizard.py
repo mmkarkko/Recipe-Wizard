b9d8273d90edb3ce6f0888919fa3667a7d7a8cdb
mmkarkko <miia.arkko@gmail.com>
2024-06-24 14:24:04 +0300

    Files created

---
+++ recipeWizard.py
@@ -0,0 +1,21 @@
+# RECIPE WIZARD
+#
+# requirements:
+# 1. write down recipes
+# 2. search recipes
+# 3. list all recipes
+# 4. print chosen recipes
+# 5. GUI
+#
+# Version 1 Miia Arkko 24.6.2024
+# --------------------------------------------------------
+# RecipeWizard RECIPE
+
+
+
+
+
+
+
+# --------------------------------------------------------
+# EOF
